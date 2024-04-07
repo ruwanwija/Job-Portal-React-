b0e2bec2d2e769940c96702f97e4c891f582ce10
ruwanwija <74661255+ruwanwija@users.noreply.github.com>
2024-04-07 17:43:13 +0530

    2024-4-7
    2024-4-7

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import HeroCards from "./components/HeroCards";
+import Hero from "./components/HeroCards";
+import Navbar from "./components/Navbar";
+
+
+const App = () => {
+
+  return(
+    <>
+    <Navbar/>
+    <Hero/>
+    <HeroCards/>
+    </>
+  );
+};
+export default App;
